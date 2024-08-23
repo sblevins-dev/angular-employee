781637e4539f5ed09453e682f5df24ca518edbf7
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-08-23 13:53:36 -0400

    updated template to a ReactiveForm

--- src/app/core/components/add-employee/add-employee.component.ts
+++ src/app/core/components/add-employee/add-employee.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { ManagerDropdownComponent } from "../../../shared/components/manager-dropdown/manager-dropdown.component";
 import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
   standalone: true,
-  imports: [ManagerDropdownComponent, RouterLink],
+  imports: [ManagerDropdownComponent, RouterLink, ReactiveFormsModule],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.css'
 })
 export class AddEmployeeComponent {
-
+  employeeForm = new FormGroup({
+    internalId: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    roles: new FormControl('')
+  })
 }
