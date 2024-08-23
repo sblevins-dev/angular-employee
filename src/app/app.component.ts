54e2a83b77a1a1b7393ca920b1011c592f698f9c
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-08-23 11:44:48 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./core/components/home/home.component";
+import { HeaderComponent } from "./core/components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
