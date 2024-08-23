d9a9330806ccaf04366fa30a7ef52f57977dbaa6
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-08-23 12:36:58 -0400

    set up home and header component. Home contains a dropdown, table, and button

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  employees = [
+    {
+      id: 1,
+      lastname: 'Smith',
+      firstname: 'Tim',
+    },
+    {
+      id: 2,
+      lastname: 'Wozoski',
+      firstname: 'Mike'
+    }
+  ]
 }
